9ac0f7daf140d090fce8615993d3ef35df6dd9f0
borba <rodrigoatjoin@gmail.com>
2022-03-19 16:20:09 -0300

    first commit

---
+++ src/main/java/com/fullcycle/CataologoVideo/CataologoVideoApplication.java
@@ -0,0 +1,13 @@
+package com.fullcycle.CataologoVideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CataologoVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CataologoVideoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullcycle/CataologoVideo/CataologoVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullcycle.CataologoVideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CataologoVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
